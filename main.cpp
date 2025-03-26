52f71bb083ac0e2059e2b2212a1c0466d3c79d07
kjo7811 <kjo7811@naver.com>
2025-03-26 17:28:10 +0900

    getDivide() and getSumSum() added

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include "gmock/gmock.h"
 
-
 class Cal {
-public:
-
+ public:
+  int getDivide(int a, int b) {
+    if (b == 0) return 0;
+    return a / b;
+  }
+  int getSumSum(int a, int b, int c) { return a + b + c; }
 };
 
 TEST(t1, t2) {
-	EXPECT_EQ(1, 1);
+  Cal a;
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(a.getDivide(10, 5), 2);
+  EXPECT_EQ(a.getSumSum(1, 2, 3), 6);
 }
 
 int main() {
-	::testing::InitGoogleMock();
-	return RUN_ALL_TESTS();
+  ::testing::InitGoogleMock();
+  return RUN_ALL_TESTS();
 }
