8eb1ee0ac71ab418115698ebe1d33891c0121b43
yuz010 <rladbwmd123@gmail.com>
2025-03-26 17:18:19 +0900

    add Zegop and Minus

--- main.cpp
+++ main.cpp
@@ -3,11 +3,21 @@
 
 class Cal {
 public:
+	int getZogop(int a) const {
+		return a * a;
+	}
 
+	int getMinus(int a, int b) const {
+		return a - b;
+	}
 };
 
 TEST(t1, t2) {
+	Cal *calculator = new Cal();
+
 	EXPECT_EQ(1, 1);
+	EXPECT_EQ(9, calculator->getZogop(3));
+	EXPECT_EQ(1, calculator->getMinus(5, 4));
 }
 
 int main() {
