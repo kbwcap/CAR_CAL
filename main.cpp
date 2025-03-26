3e2bccc4147e82d6b57ac93ac99e93c5e17ca2cc
hwayoung <raina.bak@gmail.com>
2025-03-26 17:21:58 +0900

    Add - getSum, getGop

--- main.cpp
+++ main.cpp
@@ -3,10 +3,19 @@
 
 class Cal {
 public:
+	int getSum(int a, int b) {
+		return a + b;
+	}
 
+	int getGop(int a, int b) {
+		return a * b;
+	}
 };
 
-TEST(t1, t2) {
+TEST(Cal, t2) {
+	Cal cal;
+	EXPECT_EQ(cal.getSum(1, 1), 2);
+	EXPECT_EQ(cal.getGop(1, 1), 1);
 	EXPECT_EQ(1, 1);
 }
 
